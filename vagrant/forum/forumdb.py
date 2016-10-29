eda375960d67111418036555ad52bfbf531e687d
Eunbi Go <eunbigo91@gmail.com>
2016-10-28 17:32:22 -0700

    Feat: import psycopg2 and use database

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,9 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
-import time
-
-## Database connection
-DB = []
+import psycopg2
+import bleach
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +14,13 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    #posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    conn = psycopg2.connect("dbname=forum")
+    cursor = conn.cursor()
+    cursor.execute("select time, content from posts order by time desc")
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in cursor.fetchall()]
+    conn.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +30,9 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    conn = psycopg2.connect("dbname=forum")
+    cursor = conn.cursor()
+    cursor.execute("insert into posts (content) values (%s)", (content,))
+    conn.commit()
+    conn.close()
+
